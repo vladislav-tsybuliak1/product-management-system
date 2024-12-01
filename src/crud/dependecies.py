788b3fc752704030e7d20cb69c1faece515303d2
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-12-01 13:20:13 +0200

    Create get product by id dependency

--- src/crud/dependecies.py
+++ src/crud/dependecies.py
@@ -4,8 +4,8 @@ from fastapi import HTTPException, status
 from fastapi.params import Path, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from core.models import db_helper, Category
-from crud import categories as categories_crud
+from core.models import db_helper, Category, Product
+from crud import categories as categories_crud, products as products_crud
 
 
 async def get_category_by_id(
@@ -21,4 +21,20 @@ async def get_category_by_id(
     raise HTTPException(
         status_code=status.HTTP_404_NOT_FOUND,
         detail=f"Category {category_id} not found!",
+    )
+
+
+async def get_product_by_id(
+    session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+    product_id: Annotated[int, Path],
+) -> Product:
+    product = await products_crud.get_product(
+        session=session,
+        product_id=product_id,
+    )
+    if product:
+        return product
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail=f"Product {product_id} not found!",
     )
