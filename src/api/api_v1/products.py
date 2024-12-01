f4776d30802ba138f9d6101f3adf582b3fb30788
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-12-01 04:32:39 +0200

    Add creating product view

--- src/api/api_v1/products.py
+++ src/api/api_v1/products.py
@@ -1,3 +1,21 @@
-from fastapi import APIRouter
+from typing import Annotated
+
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from core.models import db_helper, Product
+from core.schemas.product import ProductRead, ProductCreateUpdate
+from crud import products as products_crud
 
 router = APIRouter(tags=["Products"])
+
+@router.post("/", response_model=ProductRead)
+async def create_product(
+    session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+    product_create: ProductCreateUpdate,
+) -> Product:
+    product = await products_crud.create_product(
+        session=session,
+        product_create=product_create,
+    )
+    return product
