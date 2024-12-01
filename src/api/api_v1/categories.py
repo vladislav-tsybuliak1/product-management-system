fcf994c95e8398fb156a0020a558900d429bb8d3
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-12-01 04:32:12 +0200

    Add create category view

--- src/api/api_v1/categories.py
+++ src/api/api_v1/categories.py
@@ -1,5 +1,22 @@
-from fastapi import APIRouter
+from typing import Annotated
 
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from core.models import db_helper, Category
+from core.schemas.category import CategoryRead, CategoryCreateUpdate
+from crud import categories as categories_crud
 
 router = APIRouter(tags=["Categories"])
 
+
+@router.post("/", response_model=CategoryRead)
+async def create_category(
+    session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+    category_create: CategoryCreateUpdate,
+) -> Category:
+    category = await categories_crud.create_category(
+        session=session,
+        category_create=category_create,
+    )
+    return category
