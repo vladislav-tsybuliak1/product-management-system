3348a06bd9c10ac7cd6e0a4b7d490f4d8c777163
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-12-10 01:24:13 +0200

    Add configs for tests

---
+++ src/tests/api_v1/config_tests.py
@@ -0,0 +1,51 @@
+import pytest_asyncio
+from httpx import ASGITransport, AsyncClient
+from sqlalchemy import StaticPool
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+
+from core.models import db_helper, Base
+from main import main_app
+
+
+TEST_DATABASE_URL = "sqlite+aiosqlite:///:memory:"
+
+
+test_engine = create_async_engine(
+    url=TEST_DATABASE_URL,
+    echo=True,
+    connect_args={"check_same_thread": False},
+    poolclass=StaticPool,
+)
+
+TestSessionLocal = async_sessionmaker(
+    bind=test_engine,
+    autocommit=False,
+    autoflush=False,
+    expire_on_commit=False,
+)
+
+
+async def get_test_session():
+    async with TestSessionLocal() as session:
+        yield session
+
+
+main_app.dependency_overrides[db_helper.session_getter] = get_test_session
+
+
+@pytest_asyncio.fixture(scope="session", autouse=True)
+async def initialize_database():
+    async with test_engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+    async with test_engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+    await test_engine.dispose()
+
+
+@pytest_asyncio.fixture(scope="function")
+async def async_client():
+    async with AsyncClient(
+        transport=ASGITransport(app=main_app), base_url="http://test"
+    ) as client:
+        yield client
