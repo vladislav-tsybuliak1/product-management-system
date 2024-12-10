a35b24114ca2637d72c8074be6dceadeabb13d7e
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-12-10 04:03:26 +0200

    Add tests for retrieving single products

--- src/tests/api_v1/test_products.py
+++ src/tests/api_v1/test_products.py
@@ -1,17 +1,18 @@
+from itertools import product
+
 import pytest
 import pytest_asyncio
 
-from core.models import Category
+from core.models import Category, Product
 from tests.api_v1.config_tests import TestSessionLocal, async_client
 from tests.api_v1.config_tests import initialize_database
 
 
 PRODUCTS_URL = "/api/v1/products/"
-PRODUCT_ID = 1
 
 
 @pytest_asyncio.fixture(scope="session", autouse=True)
-async def category() -> Category:
+async def sample_category() -> Category:
     category = Category(name="Category for product")
     async with TestSessionLocal() as session:
         session.add(category)
@@ -21,14 +22,30 @@ async def category() -> Category:
     return category
 
 
+@pytest_asyncio.fixture(scope="session")
+async def sample_product(sample_category) -> Product:
+    product = Product(
+        name="Product 1",
+        price=1,
+        quantity=1,
+        category_id=sample_category.id,
+    )
+    async with TestSessionLocal() as session:
+        session.add(product)
+        await session.commit()
+        await session.refresh(product)
+
+    return product
+
+
 @pytest.mark.asyncio
-async def test_create_product(async_client, category: Category) -> None:
+async def test_create_product(async_client, sample_category: Category) -> None:
     product_data = {
         "name": "Test",
         "description": "Some test",
         "price": 0,
         "quantity": 0,
-        "category_id": category.id,
+        "category_id": sample_category.id,
     }
     response = await async_client.post(PRODUCTS_URL, json=product_data)
     assert response.status_code == 201
@@ -43,13 +60,13 @@ async def test_create_product(async_client, category: Category) -> None:
 @pytest.mark.asyncio
 async def test_create_product_with_negative_price(
     async_client,
-    category: Category,
+    sample_category: Category,
 ) -> None:
     product_data = {
         "name": "Test",
         "price": -1,
         "quantity": 0,
-        "category_id": category.id,
+        "category_id": sample_category.id,
     }
     response = await async_client.post(PRODUCTS_URL, json=product_data)
     assert response.status_code == 422
@@ -58,13 +75,37 @@ async def test_create_product_with_negative_price(
 @pytest.mark.asyncio
 async def test_create_product_with_negative_quantity(
     async_client,
-    category: Category,
+    sample_category: Category,
 ) -> None:
     product_data = {
         "name": "Test",
         "price": 0,
         "quantity": -1,
-        "category_id": category.id,
+        "category_id": sample_category.id,
     }
     response = await async_client.post(PRODUCTS_URL, json=product_data)
     assert response.status_code == 422
+
+
+@pytest.mark.asyncio
+async def test_get_single_product(async_client, sample_product: Product) -> None:
+    async with TestSessionLocal() as session:
+        product_from_db = await session.get(Product, sample_product.id)
+
+    response = await async_client.get(f"{PRODUCTS_URL}{sample_product.id}/")
+    assert response.status_code == 200
+    product = response.json()
+    assert product_from_db.id == product["id"]
+    assert product_from_db.name == product["name"]
+    assert product_from_db.description == product["description"]
+    assert product_from_db.price == product["price"]
+    assert product_from_db.quantity == product["quantity"]
+    assert product_from_db.category_id == product["category_id"]
+
+
+@pytest.mark.asyncio
+async def test_get_single_non_existing_product(async_client) -> None:
+    response = await async_client.get(f"{PRODUCTS_URL}999/")
+    assert response.status_code == 404
+    data = response.json()
+    assert data["detail"] == "Product 999 not found!"
